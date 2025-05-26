30bdac97df5083dcab2c7a1c4d4a9fa9571ab798
Józef Tran <jozeftran@gmail.com>
2025-05-26 18:51:00 +0200

    feat: add warning for DawidSkeneOnline

--- peerannot/models/aggregation/warnings.py
+++ peerannot/models/aggregation/warnings.py
@@ -1,7 +1,24 @@
 class DidNotConverge(RuntimeWarning):
     def __init__(
-        self, algorithm_name: str, eps: float, epsilon: float, *args,
+        self,
+        algorithm_name: str,
+        eps: float,
+        epsilon: float,
+        *args,
     ) -> None:
         super().__init__(
-            f"{algorithm_name} did not converge: err={eps}, {epsilon=}.", *args,
+            f"{algorithm_name} did not converge: err={eps}, {epsilon=}.",
+            *args,
+        )
+
+
+class NotInitialized(RuntimeWarning):
+    def __init__(
+        self,
+        algorithm_name: str,
+        *args,
+    ) -> None:
+        super().__init__(
+            f"{algorithm_name} not initialized - process at least one batch first.",
+            *args,
         )
