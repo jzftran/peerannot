8c27b25439c9560ad2969f043579258ecb27e94b
Józef Tran <jozeftran@gmail.com>
2025-09-02 10:22:18 +0200

    feat: enhance  E-step computation in VectorizedPooledFlatDiagonal

--- peerannot/models/aggregation/pooled_flat_diagonal.py
+++ peerannot/models/aggregation/pooled_flat_diagonal.py
@@ -30,3 +30,49 @@ class PooledFlatDiagonal(PooledDiagonalMultinomial):
                 T[i, j] = diag_contrib * off_diag_contrib
         self.denom_e_step = T.sum(1, keepdims=True)
         self.T = np.where(self.denom_e_step > 0, T / self.denom_e_step, T)
+
+
+class VectorizedPooledFlatDiagonal(PooledDiagonalMultinomial):
+    """iNaturalist flavour diagonal model, e_step vectorized"""
+
+    def _e_step(self) -> None:
+        # Compute worker_labels_sum: sum over workers for each task and class
+        worker_labels_sum = self.crowd_matrix.sum(
+            axis=1,
+        )  # shape (n_task, n_classes)
+
+        # Compute diag_contrib_matrix: self.pi[j] ** worker_labels_sum[i, j] for all i, j
+        diag_contrib_matrix = (
+            self.pi[None, :] ** worker_labels_sum
+        )  # shape (n_task, n_classes)
+
+        # Compute off_diag_terms
+        term_k = (1 - self.pi) * self.rho
+        denom = 1 - self.rho
+        off_diag_terms = (
+            term_k / denom[:, None]
+        )  # shape (n_classes, n_classes)
+        np.fill_diagonal(
+            off_diag_terms,
+            1,
+        )  # set diagonal to 1 to not affect the product
+
+        # Prepare for broadcasting
+        # worker_labels_sum: (n_task, n_classes) -> (n_task, 1, n_classes)
+        worker_labels_sum_expanded = worker_labels_sum[:, None, :]
+
+        # off_diag_terms: (n_classes, n_classes) -> (1, n_classes, n_classes)
+        off_diag_terms_expanded = off_diag_terms[None, :, :]
+
+        # Compute powered terms: (n_task, n_classes, n_classes)
+        powered_terms = off_diag_terms_expanded**worker_labels_sum_expanded
+
+        # Take product over k (axis=2) to get (n_task, n_classes)
+        off_diag_contrib_matrix = np.prod(powered_terms, axis=2)
+
+        # Compute T
+        T = diag_contrib_matrix * off_diag_contrib_matrix
+
+        # Normalize
+        self.denom_e_step = T.sum(axis=1, keepdims=True)
+        self.T = np.where(self.denom_e_step > 0, T / self.denom_e_step, T)
