38dda04f397f8fe3e0fb39f7373e1212535a6fbb
Józef Tran <jozeftran@gmail.com>
2026-03-02 15:11:48 +0100

    fix: add missing import statement for DawidSkeneBatch in DawidSkeneRetroactive

--- peerannot/models/aggregation/dawid_skene_retroactive.py
+++ peerannot/models/aggregation/dawid_skene_retroactive.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from peerannot.models.aggregation.dawid_skene_batch import DawidSkeneBatch
 from peerannot.models.aggregation.online_helpers import RetroactiveAlgorithm
 
 
